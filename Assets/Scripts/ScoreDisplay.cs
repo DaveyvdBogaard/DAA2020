7dae3c5e4350adad875795c957f32bee14858c37
Max van Diepen <max.vandiepen@student.fontys.nl>
2020-05-03 23:45:08 +0200

    Highscore

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    public Text playerScoreDisplay;
+    public FloatReference playerDistance;
+
+    private float highscore;
+
+    private void Update()
+    {
+        ChangeAmount();
+    }
+
+    public void ChangeAmount()
+    {
+        if (playerDistance.value > highscore)
+        {
+            highscore = playerDistance.value;
+        }
+        playerScoreDisplay.text = highscore.ToString("F1");
+    }
+}
