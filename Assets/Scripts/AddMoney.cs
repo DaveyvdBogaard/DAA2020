6108dc013ef8421984e20fd92e5f8d5bd2bb909b
Thomas <393890@student.fontys.nl>
2020-05-03 13:47:36 +0200

    money added after run, starting point fixed

---
+++ Assets/Scripts/AddMoney.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddMoney : MonoBehaviour
+{
+    public IntReference CurrentMoney;
+    public FloatReference PlayerDistance;
+    public IntReference RewardMultiplier;
+
+    public void GiveMoney()
+    {
+        int DistanceInt = Mathf.FloorToInt(PlayerDistance.value);
+        Debug.Log("give player money");
+        CurrentMoney.value += DistanceInt;
+    }
+}
